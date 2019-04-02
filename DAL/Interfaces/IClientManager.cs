950d626d97b9d0697a08cc38bf6c3e33aec3ce7b
hardy4787 <djboda97@mail.ru>
2019-04-03 01:57:19 +0300

    03.04.2019

---
+++ DAL/Interfaces/IClientManager.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IClientManager
+    {
+        void Create(ProgrammerProfile programmer);
+        ProgrammerProfile Get(string id);
+        IEnumerable<ProgrammerProfile> GetAll();
+        void Delete(string id);
+    }
+}
